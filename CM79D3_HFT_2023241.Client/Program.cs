d3ebe6a0da83127af559077ed1e3bdfb9dc87b2e
szabobazsi11182@stud.uni-obuda.hu <szabobazsi11182@stud.uni-obuda.hu>
2023-10-30 21:05:44 +0100

    DbContext letesztelve pár lekérdezéssel.

--- CM79D3_HFT_2023241.Client/Program.cs
+++ CM79D3_HFT_2023241.Client/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using CM79D3_HFT_2023241.Repository;
+using System;
+using System.Linq;
 
 namespace CM79D3_HFT_2023241.Client
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            //comment
+            FireFightingDbContext db = new FireFightingDbContext();
+            Console.WriteLine("--FIREFIGHTERS BY FIRESTATIONS--");
+            foreach (var item in db.FireStations)
+            {
+                Console.WriteLine(item.Name);
+                foreach (var item2 in item.Firefighters)
+                {
+                    Console.WriteLine("\t"+item2.FirstName + " " + item2.LastName);
+                }
+            }
+            Console.WriteLine("--EQUIPMENTS BY FIREFIGHTER--");
+            foreach (var item in db.Firefighters)
+            {
+                Console.WriteLine(item.FirstName + " " + item.LastName);
+                foreach (var item2 in item.Equipment)
+                {
+                    Console.WriteLine("\t" + item2.Type + " " + item2.Condition.ToString());
+                }
+            }
+            ;
         }
     }
 }
