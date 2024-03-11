cdebeeb2f78ca40fe3430870f584cd384f8f579a
szabobazsi11182@stud.uni-obuda.hu <szabobazsi11182@stud.uni-obuda.hu>
2024-03-11 12:25:06 +0100

    Wpf client created, with views and viewmodels (empty).

---
+++ CM79D3_GUI_2023242/ViewModels/FireStationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CM79D3_GUI_2023242.WpfClient.ViewModels
+{
+    class FireStationViewModel : ObservableRecipient
+    {
+    }
+}
---
+++ CM79D3_GUI_2023242/Views/FireStationView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CM79D3_GUI_2023242.WpfClient.Views
+{
+    /// <summary>
+    /// Interaction logic for FireStationView.xaml
+    /// </summary>
+    public partial class FireStationView : UserControl
+    {
+        public FireStationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
