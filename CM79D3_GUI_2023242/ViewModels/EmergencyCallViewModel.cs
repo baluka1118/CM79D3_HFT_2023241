3b985e04cdd60110c71c7daaeabb37e82bcb5a7d
Balázs <szabobazsi11182@stud.uni-obuda.hu>
2024-03-13 20:17:39 +0100

    MainWindow done with style, EC view works, buttons still need to be configured.

--- CM79D3_GUI_2023242/ViewModels/EmergencyCallViewModel.cs
+++ CM79D3_GUI_2023242/ViewModels/EmergencyCallViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -7,16 +8,40 @@ using System.Threading.Tasks;
 using System.Windows;
 using CM79D3_HFT_2023241.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
 
 namespace CM79D3_GUI_2023242.WpfClient.ViewModels
 {
     class EmergencyCallViewModel : ObservableRecipient
     {
         public RestCollection<EmergencyCall> EmergencyCalls { get; set; }
-
         public EmergencyCallViewModel()
         {
             EmergencyCalls = new RestCollection<EmergencyCall>("http://localhost:26947/", "emergencycall");
+        }
+        private EmergencyCall selectedItem;
+        public EmergencyCall SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                SetProperty(ref selectedItem, value);
+            }
+        }
+        public RelayCommand AddCommand { get; set; }
+        private void Add()
+        {
+        }
+
+        public RelayCommand DeleteCommand { get; set; }
+        private void Delete()
+        {
+        }
+
+        public RelayCommand UpdateCommand { get; set; }
+
+        private void Update()
+        {
         }
     }
 }
--- CM79D3_GUI_2023242/ViewModels/FireStationViewModel.cs
+++ CM79D3_GUI_2023242/ViewModels/FireStationViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CM79D3_HFT_2023241.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace CM79D3_GUI_2023242.WpfClient.ViewModels
 {
@@ -15,6 +16,30 @@ namespace CM79D3_GUI_2023242.WpfClient.ViewModels
         public FireStationViewModel()
         {
             FireStations = new RestCollection<FireStation>("http://localhost:26947/", "firestation");
+        }
+        private FireStation selectedItem;
+        public FireStation SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                SetProperty(ref selectedItem, value);
+            }
+        }
+        public RelayCommand AddCommand { get; set; }
+        private void Add()
+        {
+        }
+
+        public RelayCommand DeleteCommand { get; set; }
+        private void Delete()
+        {
+        }
+
+        public RelayCommand UpdateCommand { get; set; }
+
+        private void Update()
+        {
         }
     }
 }
