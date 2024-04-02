c7d53c6051af6b08861ddede395c8ebe706e99f2
szabobazsi11182@stud.uni-obuda.hu <szabobazsi11182@stud.uni-obuda.hu>
2024-04-02 13:24:29 +0200

    Basic fire station manager without actions done.

--- CM79D3_GUI_2023242.JSClient/wwwroot/script.js
+++ CM79D3_GUI_2023242.JSClient/wwwroot/script.js
@@ -1,5 +1,18 @@
-﻿fetch('http://localhost:26947/equipment')
+﻿let firestations = [];
+
+fetch('http://localhost:26947/firestation')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        firestations = data;
+        console.log(firestations);
+        display();
     });
+
+function display() {
+    firestations.forEach(firestation => {
+        document.getElementById('resultspart').innerHTML +=
+            '<tr><td>' + firestation.id + '</td><td>' +
+            firestation.name + '</td><td>' + firestation.location + '</td><td>' + firestation.contactInformation + '</td></tr>';
+        console.log(firestation.name);
+    });
+}
