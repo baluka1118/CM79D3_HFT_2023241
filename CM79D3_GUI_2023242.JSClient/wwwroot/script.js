c7402b654280567e513dfe9521f691d5b3015cc0
szabobazsi11182@stud.uni-obuda.hu <szabobazsi11182@stud.uni-obuda.hu>
2024-04-03 21:05:11 +0200

    Update done on firestation.

--- CM79D3_GUI_2023242.JSClient/wwwroot/script.js
+++ CM79D3_GUI_2023242.JSClient/wwwroot/script.js
@@ -7,18 +7,7 @@ FetchFireFighters();
 FetchEquipments();
 FetchEmergencyCalls();
 
-/*
-    public enum IncidentType
-    {
-        Fire,
-        MedicalEmergency,
-        Rescue,
-        HazardousMaterials,
-        NaturalDisaster,
-        FalseAlarm,
-        Other
-    }
-*/
+let idToUpdate = 0;
 const Condition = {
     0: 'New',
     1: 'Good',
@@ -78,7 +67,7 @@ async function DisplayFireStations() {
         document.getElementById('fsResults').innerHTML +=
             '<tr><td>' + firestation.id + '</td><td>' +
             firestation.name + '</td><td>' + firestation.location + '</td><td>' + firestation.contactInformation + '</td><td>' +
-            `<button type="button" onclick="removeFS(${firestation.id})">Delete</button>` + '</td><td>' + `<button type="button" onclick="ShowUpdateFS(${firestation.id})">Update</button>` + '</td></tr>';
+            `<button type="button" onclick="removeFS(${firestation.id})">Delete</button>` + '</td><td>' + `<button type="button" onclick="showUpdateFS(${firestation.id})">Update</button>` + '</td></tr>';
         console.log(firestation.name);
     });
 }
@@ -89,7 +78,7 @@ async function DisplayFireFighters() {
         document.getElementById('ffResults').innerHTML +=
             '<tr><td>' + firefighter.id + '</td><td>' +
             firefighter.firstName + '</td><td>' + firefighter.lastName + '</td><td>' + firefighter.rank + '</td><td>' + firefighter.contactInformation + '</td><td>' + firefighter.fireStation.name + '</td><td>' +
-            `<button type="button" onclick="removeFF(${firefighter.id})">Delete</button>` + '</td><td>' + `<button type="button" onclick="ShowUpdateFF(${firefighter.id})">Update</button>` + '</td></tr>';
+            `<button type="button" onclick="removeFF(${firefighter.id})">Delete</button>` + '</td><td>' + `<button type="button" onclick="showUpdateFF(${firefighter.id})">Update</button>` + '</td></tr>';
         console.log(firefighter.firstName);
     });
 }
@@ -103,7 +92,7 @@ async function DisplayEquipments() {
         document.getElementById('eqResults').innerHTML +=
             '<tr><td>' + equipment.id + '</td><td>' +
             equipment.type + '</td><td>' + condition + '</td><td>' + equipment.firefighter.firstName + ' ' + equipment.firefighter.lastName + '</td><td>' +
-            `<button type="button" onclick="removeEQ(${equipment.id})">Delete</button>` + '</td><td>' + `<button type="button" onclick="ShowUpdateEQ(${equipment.id})">Update</button>` + '</td></tr>';
+            `<button type="button" onclick="removeEQ(${equipment.id})">Delete</button>` + '</td><td>' + `<button type="button" onclick="showUpdateEQ(${equipment.id})">Update</button>` + '</td></tr>';
         console.log(equipment.type);
     });
 }
@@ -120,7 +109,7 @@ async function DisplayEmergencyCalls() {
         document.getElementById('ecResults').innerHTML +=
             '<tr><td>' + emergencycall.id + '</td><td>' +
             emergencycall.callerName + '</td><td>' + phone + '</td><td>' + emergencycall.incidentLocation + '</td><td>' + type + '</td><td>' + emergencycall.dateTime + '</td><td>' + emergencycall.fireStation.name + '</td><td>' +
-            `<button type="button" onclick="removeEC(${emergencycall.id})">Delete</button>` + '</td><td>' + `<button type="button" onclick="ShowUpdateEC(${emergencycall.id})">Update</button>` + '</td></tr>';
+            `<button type="button" onclick="removeEC(${emergencycall.id})">Delete</button>` + '</td><td>' + `<button type="button" onclick="showUpdateEC(${emergencycall.id})">Update</button>` + '</td></tr>';
         console.log(emergencycall.callerName);
     });
 }
@@ -179,12 +168,37 @@ function showAddEC() {
     });
 }
 
+function showUpdateFS(id) {
+    fireStation = firestations.find(f => f.id == id);
+    idToUpdate = id;
+    console.log(idToUpdate);
+    console.log(fireStation);
+    document.getElementById('fsNameU').value = fireStation.name;
+    document.getElementById('fsLocationU').value = fireStation.location;
+    document.getElementById('fsContactU').value = fireStation.contactInformation;
+    document.getElementById('FSUpdate').style.display = 'block';
+}
+function showUpdateFF(id) {
+    idToUpdate = id;
+    document.getElementById('FFUpdate').style.display = 'block';
+}
+function showUpdateEQ(id) {
+    idToUpdate = id;
+    document.getElementById('EQUpdate').style.display = 'block';
+}
+function showUpdateEC(id) {
+    idToUpdate = id;
+    document.getElementById('ECUpdate').style.display = 'block';
+}
+
+
 function clearResults() {
     document.getElementById('MainMenu').style.display = 'none';
     document.getElementById('FireStationsTable').style.display = 'none';
     document.getElementById('FireFightersTable').style.display = 'none';
     document.getElementById('EquipmentsTable').style.display = 'none';
     document.getElementById('EmergencyCallsTable').style.display = 'none';
+
     document.getElementById('FSAdd').style.display = 'none';
     document.getElementById('FFAdd').style.display = 'none';
     document.getElementById('EQAdd').style.display = 'none';
@@ -225,6 +239,37 @@ function addFS() {
     document.getElementById('fsLocation').value = '';
     document.getElementById('fsContact').value = '';
 }
+function updateFS() {
+    var inputName = document.getElementById('fsNameU').value;
+    var inputLocation = document.getElementById('fsLocationU').value;
+    var inputContact = document.getElementById('fsContactU').value;
+    fetch('http://localhost:26947/firestation', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(
+            { id: idToUpdate, name: inputName, location: inputLocation, contactInformation: inputContact }
+        )
+    }).then(response => {
+        if (!response.ok) {
+            return response.json();
+        }
+    }).then(data => {
+        if (data != undefined) {
+            console.log(data);
+            if (data.msg != undefined) {
+                throw new Error(data.msg);
+            }
+            if (data.status != undefined && data.status != 200) {
+                throw new Error(data.title)
+            }
+        }
+        document.getElementById('FSUpdate').style.display = 'none';
+        FetchFireStations();
+    }).catch(error => {
+        console.error(error);
+        alert(error.message);
+    });
+}
 function removeFS(id) {
     // Send a DELETE request to the server to remove the fire station with the given id
     fetch(`http://localhost:26947/firestation/${id}`, {
@@ -253,6 +298,7 @@ function removeFS(id) {
         alert(error.message);
     });
 }
+
 function addFF() {
     var inputFirstName = document.getElementById('ffFirstName').value;
     var inputLastName = document.getElementById('ffLastName').value;
@@ -291,6 +337,9 @@ function addFF() {
     document.getElementById('ffRank').value = '';
     document.getElementById('ffContact').value = '';
     document.getElementById('ffContact').value = '';
+}
+function updateFF() {
+
 }
 function removeFF(id) {
     // Send a DELETE request to the server to remove the firefighter with the given id
@@ -354,6 +403,9 @@ function addEQ() {
     document.getElementById('eqType').value = '';
     document.getElementById('eqCondition').value = '';
     document.getElementById('eqFirefighter').value = '';
+}
+function updateEQ() {
+
 }
 function removeEQ(id) {
     // Send a DELETE request to the server to remove the equipment with the given id
@@ -422,6 +474,9 @@ function addEC() {
     document.getElementById('ecDate').value = '';
     document.getElementById('ecIncidentType').value = '';
     document.getElementById('ecFireStation').value = '';
+}
+function updateEC() {
+
 }
 function removeEC(id) {
     // Send a DELETE request to the server to remove the emergency call with the given id
